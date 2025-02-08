4824eed10597ec1850bbf1a09c26e3b8e0e15f86
Roshan Yadav <yroshan504@gmail.com>
2025-02-08 16:59:49 +0530

    login and signup

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -8,6 +8,9 @@ export const signup = async (req,res) => {
         if (!fullName || !email || !password || !confirmPassword){
             return res.status(400).json({message:"All fields are required"});
         }
+        if(password !== confirmPassword){
+            return res.status(400).json({message:"Password and confirm password should match"})
+        }
         if (password.length < 6){
             return res.status(400).json({message:"Password must be at leat 6 characters"});
         }
@@ -43,3 +46,41 @@ export const signup = async (req,res) => {
         res.status(500).json({message:"Internal Server Error"})
     }
 };
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        // Directly comparing the passwords (not recommended in production)
+        if (password !== user.password) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        // Generate and send the JWT token if passwords match
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+          
+        });
+    } catch (err) {
+        console.log("Error in login controller:", err.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const checkAuth = (req,res) => {
+    try{
+        res.status(200).json(req.user);
+    } catch(err){
+        console.log("Error in checkAuth controller",err)
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { signup } from "../controllers/auth.js";
+import { checkAuth, login, signup } from "../controllers/auth.js";
+import { protectRoute } from "../middleware/authCheck.js";
 
 
 const router = express.Router();
 
 router.post("/signup", signup);
-// router.post("/login", login);
+router.post("/login", login);
 // router.post("/logout", logout);
 
-// router.get("/check",protectRoute,checkAuth)
+router.get("/check",protectRoute,checkAuth)
 
 export default router;
